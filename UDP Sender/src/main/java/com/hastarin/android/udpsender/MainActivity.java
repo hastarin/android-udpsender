9a9bcb99dbc65c52ac9996e7832adce11e6709f2
hastarin <hastarin+github@gmail.com>
2014-01-28 11:47:15 +1100

    Added Send command to the menu.  Removed Settings command that did nothing.

--- UDP Sender/src/main/java/com/hastarin/android/udpsender/MainActivity.java
+++ UDP Sender/src/main/java/com/hastarin/android/udpsender/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -24,6 +25,18 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_send:
+                this.sendData(null);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 	public void sendData(View view) {
 		Context context = getApplicationContext();
